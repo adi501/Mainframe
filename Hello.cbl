9147410d95173ad9f40da72e7576166656445f5e
Adinarayana Reddy JC <jc.adi101@gmail.com>
2021-09-25 22:35:31 +0530

    Add files via upload
    hello world program in cobol

---
+++ Hello.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:Adi
+      * Date:09/25/2021
+      * Purpose: Its Hello world program
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Hello.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM Hello.
