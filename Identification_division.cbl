defad977aa0dfae87fbec7d7a06852cb8bac8b0f
Adinarayana Reddy JC <jc.adi101@gmail.com>
2021-10-11 22:02:24 +0530

    Add files via upload

---
+++ Identification_division.cbl
@@ -0,0 +1,15 @@
+      ******************************************************************
+      * Author:Adi
+      * Date:10/11/2021
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. myProgram1.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Identification division in cobol"
+            STOP RUN.
---
+++ firstCobol.cbl
@@ -0,0 +1,16 @@
+      ******************************************************************
+      * Author:Adi
+      * Date:10/11/2021
+      * Purpose:Hello world program
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. YOUR-PROGRAM-NAME.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "Hello world"
+            STOP RUN.
+       END PROGRAM YOUR-PROGRAM-NAME.
